5fb230b9fd3ffada47ba20b50935ad78fade45a3
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-29 23:32:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,7 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
+        alans_new_info = "GUI"
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
