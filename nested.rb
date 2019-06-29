e4a1ec1cc7a4e80a6163b74bf9c18da9d6605dd0
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-29 23:26:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,7 +100,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => [alans_new_info],
+          :known_for => "alans_new_info",
           :languages => ["Smalltalk", "LISP"]
           
         },
@@ -113,7 +113,7 @@ def changing_alan
      alans_new_info = "GUI"
      
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash
+    
 end
 
 def adding_to_dennis
